27d0de2a449155f61324cb4332ee3f0bf41f9b20
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-15 19:53:12 +0100

    styled modal background

--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -18,7 +18,7 @@ export default function BasicSpeedDial() {
     left: "50%",
     transform: "translate(-50%, -50%)",
     width: 400,
-    bgcolor: "background.paper",
+    bgcolor: "white",
     border: "2px solid #000",
     boxShadow: 24,
     p: 4,
