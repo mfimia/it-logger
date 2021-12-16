6fd9f1f5b41e0b2092c7cbf6399102c21b2d41c7
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-16 14:36:34 +0100

    styled speed dial

--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -1,60 +1,37 @@
-import { Button } from "@mui/material";
 import SpeedDialIcon from "@mui/material/SpeedDialIcon";
-import { Fragment, useState } from "react";
-import Modal from "@mui/material/Modal";
-import { Box } from "@mui/system";
-import { Typography } from "@mui/material";
+import { useState } from "react";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import PersonIcon from "@mui/icons-material/Person";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+
+const actions = [
+  { icon: <PersonIcon color="primary" />, name: "People" },
+  { icon: <PersonAddIcon color="success" />, name: "Add" },
+];
 
 export default function BasicSpeedDial() {
   const [open, setOpen] = useState(false);
-  const handleOpen = () => {
-    setOpen(true);
-  };
+  const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "white",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
-
   return (
-    <Fragment>
-      <Button
-        variant="contained"
-        size="medium"
-        sx={{
-          position: "absolute",
-          bottom: 24,
-          right: 24,
-          borderRadius: "50%",
-          height: 60,
-        }}
-        onClick={handleOpen}
-      >
-        <SpeedDialIcon />
-      </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </Fragment>
+    <SpeedDial
+      ariaLabel="SpeedDial"
+      sx={{ position: "absolute", bottom: 16, right: 16 }}
+      icon={<SpeedDialIcon />}
+      onClose={handleClose}
+      onOpen={handleOpen}
+      open={open}
+    >
+      {actions.map((action) => (
+        <SpeedDialAction
+          key={action.name}
+          icon={action.icon}
+          tooltipTitle={action.name}
+          onClick={handleClose}
+        />
+      ))}
+    </SpeedDial>
   );
 }
