479038a239b1af8186cb471c89eac648db32fd33
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-16 19:17:54 +0100

    log form set up with modal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import { Fragment } from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Logs from "./components/logs/Logs";
 import SearchBar from "./components/layout/SearchBar";
 import { Box } from "@mui/system";
 import AddBtn from "./components/layout/AddBtn";
+// import NotFound from "./pages/NotFound";
+// import AddLogModal from "./components/logs/AddLogModal";
 
 const App = () => {
   return (
     <Fragment>
+      {/* <Router> */}
       <SearchBar />
       <Box sx={{ flexGrow: 1, maxWidth: 600 }}>
         <Logs />
       </Box>
       <AddBtn />
+      {/* <Routes>
+          <Route path="/add/log" element={<AddLogModal />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router> */}
     </Fragment>
   );
 };
--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -1,37 +1,62 @@
 import SpeedDialIcon from "@mui/material/SpeedDialIcon";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import SpeedDial from "@mui/material/SpeedDial";
 import SpeedDialAction from "@mui/material/SpeedDialAction";
 import PersonIcon from "@mui/icons-material/Person";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
-
-const actions = [
-  { icon: <PersonIcon color="primary" />, name: "People" },
-  { icon: <PersonAddIcon color="success" />, name: "Add" },
-];
+import AddLogModal from "../logs/AddLogModal";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import { Link } from "react-router-dom";
 
 export default function BasicSpeedDial() {
   const [open, setOpen] = useState(false);
+  const [addModal, setAddModal] = useState(false);
+  const [techs, setTechs] = useState(false);
+  const [addTech, setAddTech] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const actions = [
+    {
+      icon: <PersonIcon color="primary" />,
+      name: "Techs",
+      click: () => console.log("hi people"),
+    },
+    {
+      icon: <PersonAddIcon color="success" />,
+      name: "Add Tech",
+      click: () => console.log("hi add"),
+    },
+    {
+      icon: <AddBoxIcon color="secondary" />,
+      name: "Add Log",
+      click: () => setAddModal(true),
+    },
+  ];
+
   return (
-    <SpeedDial
-      ariaLabel="SpeedDial"
-      sx={{ position: "absolute", bottom: 16, right: 16 }}
-      icon={<SpeedDialIcon />}
-      onClose={handleClose}
-      onOpen={handleOpen}
-      open={open}
-    >
-      {actions.map((action) => (
-        <SpeedDialAction
-          key={action.name}
-          icon={action.icon}
-          tooltipTitle={action.name}
-          onClick={handleClose}
-        />
-      ))}
-    </SpeedDial>
+    <Fragment>
+      <SpeedDial
+        ariaLabel="SpeedDial"
+        sx={{ position: "absolute", bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+        id="name"
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            onClick={action.click}
+          />
+        ))}
+      </SpeedDial>
+      {addModal && (
+        <AddLogModal addModal={addModal} setAddModal={setAddModal} />
+      )}
+    </Fragment>
   );
 }
