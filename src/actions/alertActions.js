422f3c475d662d0e43562d8f30bad8d807fbddc0
Fimia Osuna <martin.fimiaosuna@here.com>
2021-12-19 08:58:27 -0600

    set up remove alert and set alert functions

--- src/actions/alertActions.js
+++ src/actions/alertActions.js
@@ -1,1 +1,12 @@
 import { SET_ALERT, REMOVE_ALERT } from "./types";
+
+// Fire alert
+export const setAlert = (msg, type, temp = false) => {
+  dispatch({ type: SET_ALERT, payload: { msg, type } });
+  temp && setTimeout(() => dispatch({ type: REMOVE_ALERT }), 3000);
+};
+
+// Clear alert
+export const clearAlert = () => {
+  dispatch({ type: REMOVE_ALERT });
+};
--- src/reducers/alertReducer.js
+++ src/reducers/alertReducer.js
@@ -8,6 +8,10 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_ALERT:
+      return action.payload;
+    case REMOVE_ALERT:
+      return null;
     default:
       return state;
   }
