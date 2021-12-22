6a8b88ed3dd4c2dee6d33463f8653afac26cd02c
Fimia Osuna <martin.fimia.osuna@hotmail.com>
2021-12-22 08:32:31 +0100

    removed icon button from navbar

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -5,10 +5,8 @@ import { searchLogs } from "../../actions/logActions";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
-import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 
 const Search = styled("div")(({ theme }) => ({
@@ -60,15 +58,6 @@ const SearchAppBar = ({ searchLogs }) => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
             variant="h6"
             noWrap
