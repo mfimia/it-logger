8a2670fd19af84b71944cb865b2381be1aad8a01
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-02 21:14:46 +0100

    set up put and delete apis

--- routes/logs.js
+++ routes/logs.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 const express = require("express");
 const router = express.Router();
 const dotenv = require("dotenv");
@@ -18,8 +19,8 @@ router.get("/", async (req, res) => {
     client.connect(async () => {
       const db = client.db("ITLogger");
       const logs = await db.collection("Logs").find().toArray();
-      client.close();
       res.json(logs);
+      await client.close();
     });
   } catch (err) {
     console.error(err.message);
@@ -44,8 +45,60 @@ router.post("/", async (req, res) => {
     client.connect(async () => {
       const db = client.db("ITLogger");
       await db.collection("Logs").insertOne(newLog);
-      client.close();
       res.json(newLog);
+      await client.close();
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// This route will allow to delete a log
+// @route   DELETE api/logs
+// desc     Delete log
+// @access  Private
+router.delete("/:id", async (req, res) => {
+  try {
+    client.connect(async () => {
+      const db = client.db("ITLogger");
+      await db.collection("Logs").deleteOne({ _id: ObjectId(req.params.id) });
+      const logs = await db.collection("Logs").find().toArray();
+      res.json(logs);
+      await client.close();
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// This route will allow to update a log
+// @route   update api/logs
+// desc     update log
+// @access  Private
+router.put("/:id", async (req, res) => {
+  try {
+    const { message, attention, techSelected, date } = await req.body;
+    const log = {
+      message,
+      attention,
+      techSelected,
+      date,
+    };
+    client.connect(async () => {
+      const db = client.db("ITLogger");
+      await db
+        .collection("Logs")
+        .updateOne({ _id: ObjectId(req.params.id) }, { $set: log });
+
+      const sentLog = await db
+        .collection("Logs")
+        .findOne({ _id: ObjectId(req.params.id) });
+
+      const logs = await db.collection("Logs").find().toArray();
+      res.json({ sentLog, logs });
+      await client.close();
     });
   } catch (err) {
     console.error(err.message);
